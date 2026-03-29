38a1cc0ded6742b54cafe401df6fcd9553322de7
Mahammad Mufeez <123565073+mufeez123-lab@users.noreply.github.com>
2026-03-29 05:19:23 +0000

    main

--- luxe-coords-web/src/components/Hero.jsx
+++ luxe-coords-web/src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => (
   <section className="relative h-screen flex items-center justify-center overflow-hidden">
     <div className="absolute inset-0 z-0">
       <img 
-        src="https://images.unsplash.com/photo-1490481651871-ab68de25d43d?auto=format&fit=crop&q=80" 
+        src="/heroimage1.webp" 
         className="w-full h-full object-cover brightness-75"
         alt="Luxe fashion"
       />
@@ -18,12 +18,13 @@ const Hero = () => (
 >
   {/* Added 'flex justify-center' to ensure the span contents are centered */}
   <span className="uppercase tracking-[0.3em] text-sm mb-4  flex justify-center">
-    <img src="/lux1.png" alt="" className='h-20 rounded-[20px]'/>
+    {/* <img src="/lux1.png" alt="" className='h-20 rounded-[20px]'/> */}
+    <p>Luxe Coords Signature Collections</p>
   </span>
   
   <h2 className="text-5xl md:text-7xl font-serif mb-8 italic">Effortless Elegance</h2>
   
-<a href="#products">  <button className="bg-white text-black px-8 py-4 uppercase tracking-widest text-sm hover:bg-luxe-gold hover:text-white transition-all duration-300">
+<a href="#products">  <button className="bg-white text-black px-8 py-4 uppercase tracking-widest text-sm hover:bg-luxe-gold hover:rounded-[20px] hover:text-blaack transition-all duration-300">
     Explore Sets
   </button></a>
 </motion.div>
--- luxe-coords-web/src/components/Navbar.jsx
+++ luxe-coords-web/src/components/Navbar.jsx
@@ -2,12 +2,22 @@ import React from 'react';
 import { ShoppingBag, Menu } from 'lucide-react';
 
 const Navbar = () => (
-  <nav className="fixed w-full z-50 bg-white/80 backdrop-blur-md border-b border-gray-100 px-6 py-2 flex justify-center items-center">
-    <img src="/luxew.png" alt="" className='h-20'/>
+  <nav className="fixed w-full z-50 h-24 border-b border-gray-100 px-6 flex justify-center items-center overflow-hidden">
+    
+    {/* Background Image Tag */}
+    <img 
+      src="/120.jpg" 
+      alt="Background" 
+      className="absolute inset-0 w-full h-full object-cover -z-20"
+    />
 
-  
+    {/* Optional Overlay/Blur for readability */}
+    <div className="absolute inset-0 bg-white/50  -z-10"></div>
+    
+    {/* Logo */}
+    <img src="/luxew.png" alt="Luxe Logo" className="h-20 z-10 relative"/>
+
   </nav>
-  
 );
 
 export default Navbar;
